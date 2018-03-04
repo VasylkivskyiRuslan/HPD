df116cc3b864859b76440f9c511774b42a56e22d
VasylkivskyiRuslan <vasylkivskyi.ruslan@gmail.com>
2018-03-04 18:33:22 +0200

    small changes

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
-$('.social__mobile ul').hide();
+(function($){
+    $( '.social__mobile ul' ).hide();
 
-$('#socialBar').click(function(){
-    $('.social__mobile ul').slideToggle('normal');
-});
+    $( '#socialBar' ).click(function(){
+        $( '.social__mobile ul' ).slideToggle( 'slow' );
+    });
 
-
+    $(document).on( 'click', function(e) {
+        if (!$(e.target).closest( '.social__mobile' ).length) {
+            $( '.social__mobile ul' ).hide();
+        }
+        e.stopPropagation();
+    });
+})(jQuery);
