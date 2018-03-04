a2bad3c44ab82810ba110c0f3fc24562f8d3ea4a
VasylkivskyiRuslan <vasylkivskyi.ruslan@gmail.com>
2018-03-04 21:40:01 +0200

    little changes

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@
         $('.aboutStudent__head').fadeTo(2500, 1);
         $('.info').animate({left: 0}, 1500);
         $('.expirience').animate({right: 0}, 2000);
-        $('.courses__head').fadeTo(6000, 1);
-        $('.courses__type').animate({top: 0}, 3000);
+        $('.courses__head').fadeTo(2500, 1);
+        $('.courses__type').animate({top: 0}, 1500);
     });
 
     $( '.socialMobile ul' ).hide();
