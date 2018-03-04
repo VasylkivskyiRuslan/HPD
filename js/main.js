53ae90bdcb3c779c93baad0b5ada869b230d0880
VasylkivskyiRuslan <vasylkivskyi.ruslan@gmail.com>
2018-03-04 20:25:37 +0200

    add animation to info and expirience div

--- js/main.js
+++ js/main.js
@@ -1,13 +1,20 @@
 (function($){
-    $( '.social__mobile ul' ).hide();
+    $(document).ready(function(){
+        $('.aboutStudent__head').fadeTo(2500, 1);
+        $('.courses__head').fadeTo(2500, 1);
+        $('.info').animate({left: 0}, 1500);
+        $('.expirience').animate({right: 0}, 2000);
+    });
+
+    $( '.socialMobile ul' ).hide();
 
     $( '#socialBar' ).click(function(){
-        $( '.social__mobile ul' ).slideToggle( 'slow' );
+        $( '.socialMobile ul' ).slideToggle( 'slow' );
     });
 
     $(document).on( 'click', function(e) {
-        if (!$(e.target).closest( '.social__mobile' ).length) {
-            $( '.social__mobile ul' ).hide();
+        if (!$(e.target).closest( '.socialMobile' ).length) {
+            $( '.socialMobile ul' ).hide();
         }
         e.stopPropagation();
     });
